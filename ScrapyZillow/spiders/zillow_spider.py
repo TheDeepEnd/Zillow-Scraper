8c3c17f5a67358b71276c11377994bcc25efd552
Tyler Nakamura <tyler@tylernakamura.com>
2016-03-17 19:06:30 -1000

    grab all links of properties from an index page

--- ScrapyZillow/spiders/zillow_spider.py
+++ ScrapyZillow/spiders/zillow_spider.py
@@ -9,15 +9,17 @@ class ZillowSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        #instantiate selector with body of html page
         sel = Selector(response)
-        prices = sel.xpath('//*[contains(concat(" ", normalize-space(@span), " "), " price ")]')
-        items = []
 
-        for price in prices:
-            items.append(price)
-            print price
-        
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(items)
+        #select all links to properties listed on the page
+        links = sel.xpath("//a[contains(@class,'hdp-link routable')]/@href").extract()
+
+        #append 'zillow.com' to the links, as they are incomplete
+        #print all links
+        for link in links:
+            ret = "zillow.com" + link
+            link = ret
+            print link
+
         
